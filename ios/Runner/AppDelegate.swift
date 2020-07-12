1a82790d627135a94a2ff3387af82d34ebf130ea
davidionita <31045833+davidionita@users.noreply.github.com>
2020-07-12 01:33:29 -0400

    Working config shell script for mac

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,9 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool { 
-    // GMSServices.provideAPIKey("GOOGLE_MAPS_API_KEY")
-    // **Exactly** as shown in the sample above, pass the GOOGLE_MAPS_API_KEY from .env into the function below
-    let googleMapsAPIKey = "GOOGLE_MAPS_API_KEY"
+    let googleMapsAPIKey = GOOGLE_MAPS_API_KEY
     GMSServices.provideAPIKey(googleMapsAPIKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
