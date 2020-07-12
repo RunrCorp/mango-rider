95ac0e9cb876018086d4262540d13dde6184a016
davidionita <31045833+davidionita@users.noreply.github.com>
2020-07-12 03:25:12 -0400

    Shell script will now throw error if .env is not set up and will automatically set API key on first run

--- .gitconfig.sh
+++ .gitconfig.sh
@@ -3,18 +3,45 @@
 # Set colors
 GREEN='\033[1;32m'
 CYAN='\033[1;36m'
+RED='\033[1;31m'
 NC='\033[0m'
 
 printf "${GREEN}----${NC}"
 echo
-
 # Set .env variables
 export $(grep -v '^#' .env | xargs -0)
+if [ -z ${GOOGLE_MAPS_API_KEY+x} ]
+then
+    printf "${GREEN}----${NC}"
+    echo
+
+    printf "${RED}ERROR: Please set up .env file before running.${NC}"
+    echo
+
+    printf "${CYAN}Stopping ...${NC}"
+    echo
+
+    unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs -0)
+
+    exit
+
+else 
+    printf "${CYAN}.env file found!${NC}"
+    echo
+
+    printf "${GREEN}----${NC}"
+    echo
+
+fi
+
 printf "${GREEN}GOOGLE_MAPS_API_KEY=${GOOGLE_MAPS_API_KEY}${NC}"
 echo
 
 printf "${GREEN}----${NC}"
 echo
+
+# Inserts API key into file
+sed -i '' 's/googleMapsAPIKey = .*/googleMapsAPIKey = "'${GOOGLE_MAPS_API_KEY}'"/' 'ios/Runner/AppDelegate.swift'
 
 # -- Set custom local .gitconfig --
 
@@ -28,3 +55,5 @@ printf "${GREEN}----${NC}"
 
 # Unset .env variables
 unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs -0)
+
+exit
